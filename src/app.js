33f68fdaa5bc2ef4dc8b219bbfc8637b4f913829
Vincent Harvey <vharv008@uottawa.ca>
2018-11-06 15:32:05 -0500

    Setting up the firebase database behind all of it.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
+import './firebase/firebase';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
