18d30f937c126bb6028d1a301caf71e5c26d83cd
Vincent Harvey <vharv008@uottawa.ca>
2018-11-08 15:58:56 -0500

    Setting up private/public routes.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,21 +3,20 @@ import { Router, Route, Switch } from 'react-router-dom';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import HelpPage from '../components/HelpPage';
 import Login from '../components/Login';
 import NotFoundPage from '../components/NotFoundPage';
 import { history } from '../helpers/history.helper';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={Login} exact={true} />
+        <PublicRoute path="/" component={Login} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
